f420ef6f2e02aac5a30b3cd39c41d21f87710130
Cshauli <s631831@student.roundrockisd.org>
2014-11-20 15:23:04 -0600

    questions

---
+++ python questions 2.py
@@ -0,0 +1,269 @@
+a = ' '
+b = ' '
+c = ' '
+d = ' '
+e = ' '
+f = ' '
+g = ' '
+h = ' '
+i = ' '
+j = ' '
+k = ' '
+l = ' '
+m = ' '
+n = ' '
+o = ' '
+p = ' '
+q = ' '
+r = ' '
+s = ' '
+t = ' '
+u = 'Otters sleep while holding hands.'
+v = ''
+w = ' '
+x = ' '
+y = ' '
+z = ' '
+aa = ' '
+bb = ' '
+cc = ' '
+dd = ' '
+ee = ' '
+ff = ' '
+gg = ' '
+hh = ' '
+ii = ' '
+jj = ' '
+kk = ' '
+ll = ' '
+mm = ' '
+nn = ' '
+oo = ' '
+pp = ' '
+qq = ' '
+rr = ' '
+ss = ' '
+tt = ' '
+uu = ' '
+vv = ' '
+ww = ' '
+xx = ' '
+qList = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,aa,bb,cc,dd,ee,ff,gg,hh,ii,jj,kk,ll,mm,nn,oo,pp,qq,rr,ss,tt,uu,vv,ww,xx]
+print 'Here is your 21st question.'
+print qList[20]
+userInput = raw_input()
+if userInput == 't':
+    print 'Correct!'
+else:
+    print "I'm sorry, that is not correct"
+print 'Here is your question.'
+print qList[21]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 22nd question.'
+print qList[22]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 23rd question.'
+print qList[23]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 24th question.'
+print qList[24]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 25th question.'
+print qList[25]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 26th question.'
+print qList[26]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 27th question.'
+print qList[27]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 28th question.'
+print qList[28]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 29th question.'
+print qList[29]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 30th question.'
+print qList[30]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 31st question.'
+print qList[31]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 32nd question.'
+print qList[32]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 33rd question.'
+print qList[33]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 34th question.'
+print qList[20]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 35th question.'
+print qList[34]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 36th question.'
+print qList[35]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 37th question.'
+print qList[36]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 38th question.'
+print qList[37]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 39th question.'
+print qList[38]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 40th question.'
+print qList[39]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 41st question.'
+print qList[40]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 42nd question.'
+print qList[41]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 43rd question.'
+print qList[42]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 44th question.'
+print qList[43]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 45th question.'
+print qList[44]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 46th question.'
+print qList[45]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 47th question.'
+print qList[46]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 48th question.'
+print qList[47]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 49th question.'
+print qList[48]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+print 'Here is your 50th question.'
+print qList[49]
+userInput = raw_input()
+if userInput == '':
+    print 'Correct!'
+else:
+    print ' '
+    
